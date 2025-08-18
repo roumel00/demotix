3010ff996268647dfe7e63320bdf6d5bb295197a
Jake <jakedoyle8@gmail.com>
2025-08-18 15:35:14 +1000

    renaming link

--- components/component-showcase.jsx
+++ components/component-showcase.jsx
@@ -17,7 +17,7 @@ export default function ComponentShowcase() {
       <p className="text-muted-foreground text-lg">
         <a href="https://ui.shadcn.com/components" target="_blank" rel="noopener noreferrer" className="text-blue-500 border-b border-transparent hover:border-blue-500 transition-all duration-200 ease-in-out">ui.shadcn.com</a> contains the library of base UI components.<br />
         <a href="https://shadcn.io/components" target="_blank" rel="noopener noreferrer" className="text-blue-500 border-b border-transparent hover:border-blue-500 transition-all duration-200 ease-in-out">shadcn.io</a> contains improved components of greater complexity.<br />
-        See the full repository of this demo page <a href="https://github.com/roumel00/jaketix" target="_blank" rel="noopener noreferrer" className="text-blue-500 border-b border-transparent hover:border-blue-500 transition-all duration-200 ease-in-out">here</a>.
+        See the full repository of this demo page <a href="https://github.com/roumel00/demotix" target="_blank" rel="noopener noreferrer" className="text-blue-500 border-b border-transparent hover:border-blue-500 transition-all duration-200 ease-in-out">here</a>.
       </p>
     </div>
   )
